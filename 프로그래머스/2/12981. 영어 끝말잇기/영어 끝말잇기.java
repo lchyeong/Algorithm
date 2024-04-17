ed7641e48de27a17bd1a93e8f470eee1e60aa09d
lchyeong <164587591+lchyeong@users.noreply.github.com>
2024-04-17 22:18:15 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.28 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class Solution {
         public int[] solution(int n, String[] words) {
         //GPT 풀이.......;;;
-            
+         /*   
         int[] answer = {0, 0};
         Set<String> seenWords = new HashSet<>(); // To store words that have been seen
         seenWords.add(words[0]); // Adding the first word to the set
@@ -34,17 +34,17 @@ public class Solution {
         return answer;
             
             
-            
+           */ 
             
             
             
             //나의 풀이
-            /* int[] answer = {0,0};
+            int[] answer = {0,0};
             List<String> li = new ArrayList<>();
             List<String> li2 = new ArrayList<>();
             Collections.addAll(li, words);
             int len = li.size();
-            int cnt = 1;
+            //int cnt = 1;
 
 
             for(int i=0 ; i<len ; i++){
@@ -52,9 +52,9 @@ public class Solution {
 
                 //같은 단어를 말한 경우.
                 if( li2.contains(str) && (li2.size() > 1) ){
-                    int x = (li2.size()+1)-(n*cnt) + n;
-                    answer[0] =x;
-                    answer[1] =cnt;
+                    //int x = (li2.size()+1)-(n*cnt) + n;
+                    answer[0] =i%n+1;
+                    answer[1] =i/n+1;
                     break;
                 }
 
@@ -64,17 +64,17 @@ public class Solution {
                 if(i>0){
                     String c = li2.get(i-1);
                     if(c.charAt(c.length()-1) != str.charAt(0)){
-                        int x = li2.size()-(n*cnt) + n;
-                        answer[0] =x;
-                        answer[1] =cnt;
+                       // int x = li2.size()-(n*cnt) + n;
+                        answer[0] =i%n+1;
+                        answer[1] =i/n+1;
                         break;
                     }
                 }
-                if(i>0 && i == (cnt*n - 1)){
-                    cnt++;
-                }
+             //   if(i>0 && i == (cnt*n - 1)){
+             //       cnt++;
+             //   }
             }
             return answer;
-            */
+        
     }
 }
