3294ca1ec58f6abd7343541248fe69610a964031
lchyeong <lchyeong@gmail.com>
2024-05-05 10:42:41 +0900

    0505

--- 파이썬 알고리즘/연속한 숫자 합 구하기2.py
+++ 파이썬 알고리즘/연속한 숫자 합 구하기2.py
@@ -1,7 +1,7 @@
 n = int(input("number : "))
 
 
-def sum_n(n):
+def sum_n(x):
     s = n * (n + 1) / 2
     return s
 
---
+++ 파이썬 알고리즘/최댓값 구하기1.py
@@ -0,0 +1,12 @@
+li = [17, 92, 18, 33, 58, 7, 33, 42]
+
+
+def find_max(li):
+    for i in range(1, len(li)):
+        maxNum = li[0]
+        if (maxNum < li[i]):
+            maxNum = li[i]
+        return maxNum
+
+
+print(find_max(li))R
