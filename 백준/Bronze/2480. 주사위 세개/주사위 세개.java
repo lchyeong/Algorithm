288328937fc18ff94b279e4e3116148768776fd3
LEE CHAN HYEONG <164587591+lchyeong@users.noreply.github.com>
2024-05-24 15:11:56 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+
+        System.out.println(dice(a, b, c));
+        sc.close();
+
+    }
+
+    static int dice(int a, int b, int c){
+        if( a == b && b == c){
+            return 10000 + a*1000;
+        }else if( a == b || a == c || b == c){
+            if (a == b || a == c) {
+               return 1000 + a*100;
+            }
+            return 1000 + b*100;
+        }else{
+            int max = Math.max(a, Math.max(b, c));
+            return max*100;
+        }
+    }
+}
