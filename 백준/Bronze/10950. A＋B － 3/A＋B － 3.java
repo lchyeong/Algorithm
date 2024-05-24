36bd29dd9108f28641b6c56044c259b6f0a59149
LEE CHAN HYEONG <164587591+lchyeong@users.noreply.github.com>
2024-05-24 15:23:16 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17788 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -5,19 +5,22 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        // 스캐너 자원을 사용하고 나면 close() 메소드를 호출하여 닫아줘야 한다.
+        try {
+            int n = sc.nextInt();
+            int[] arr = new int[n]; 
 
-        int n = sc.nextInt();
-        int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                arr[i] = a + b;
+            }
 
-        for(int i = 0; i <n ; i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            arr[i] = a+b;
+            for (int i : arr) {
+                System.out.println(i);
+            }
+        } finally {
+            sc.close();
         }
-
-        for (int i : arr) {
-            System.out.println(i);
-        }
-
     }
 }
