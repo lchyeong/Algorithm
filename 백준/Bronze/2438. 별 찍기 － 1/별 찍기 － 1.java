6031f4964760f615fe3dabb7bc1aa464db23f0f5
LEE CHAN HYEONG <164587591+lchyeong@users.noreply.github.com>
2024-10-23 21:50:34 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testCaseCount = Integer.parseInt(bufferedReader.readLine());
+
+
+        for(int i=0; i<testCaseCount; i++){
+            for(int j = 0;j<=i; j++){
+                bufferedWriter.write("*");
+            }
+            bufferedWriter.write("\n");
+        }
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
