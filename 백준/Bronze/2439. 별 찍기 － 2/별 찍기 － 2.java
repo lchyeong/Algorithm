a9d0298271b461f740974085299eb2748a61cbe6
LEE CHAN HYEONG <164587591+lchyeong@users.noreply.github.com>
2024-10-24 23:44:29 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,26 +1,28 @@
-
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int testCaseCount = Integer.parseInt(bufferedReader.readLine());
 
-        for(int i=0; i<testCaseCount; i++){
+        for (int i = 0; i < testCaseCount; i++) {
+            StringBuilder sb = new StringBuilder();
 
-            for(int k = testCaseCount-i-1 ; k>0 ; k--){
-                bufferedWriter.write(" ");
+            for (int k = testCaseCount - i - 1; k > 0; k--) {
+                sb.append(" ");
             }
 
-            for(int j = 0 ; j<=i ; j++){
-                bufferedWriter.write("*");
+            for (int j = 0; j <= i; j++) {
+                sb.append("*");
             }
-            bufferedWriter.write("\n");
+
+            sb.append("\n");
+
+            bufferedWriter.write(sb.toString());
         }
+
         bufferedWriter.flush();
         bufferedWriter.close();
     }
